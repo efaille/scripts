6f7d0ca7c0211059623ed19b012da189a2e131ea
Erick Faille <efaille@gmail.com>
2015-08-19 21:42:36 -0400

    test

--- setup-all.sh
+++ setup-all.sh
@@ -4,8 +4,12 @@ install_docker() {
   curl -sSL https://get.docker.com/ | sh
 }
 
+pull_docker_images() {
+  docker pull efaille/ubuntu
+}
+
 setup() {
-  install_docker
+  pull_docker_images
 }
 
 setup
