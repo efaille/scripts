6be61eb7aa74220d509a15d7660c63d89b85c6fa
Erick Faille <efaille@gmail.com>
2015-08-19 22:07:09 -0400

    test

--- setup-all.sh
+++ setup-all.sh
@@ -6,6 +6,16 @@ install_docker() {
 
 pull_docker_images() {
   docker pull efaille/ubuntu
+  docker pull efaille/java
+  docker pull efaille/nginx
+  docker pull efaille/mongodb
+  docker pull efaille/iojs
+  docker pull efaille/maven
+  docker pull efaille/minecraft
+  docker pull efaille/spring-boot
+  docker pull efaille/nginx-angularjs
+  docker pull efaille/nginx-angularjs-api
+  docker pull efaille/nginx-proxy
 }
 
 setup() {
