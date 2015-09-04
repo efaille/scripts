b30cb713e41252098066b0c2ad31014498da0084
Erick Faille <efaille@gmail.com>
2015-09-03 22:33:22 -0400

    test

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -9,7 +9,7 @@ install_ssh() {
 }
 
 configure_ssh() {
-  sed -i -e "s/;PermitRootLogin\s*=\s*without-password/PermitRootLogin = yes/g" /etc/ssh/sshd_config
+  sed -i -e "s/PermitRootLogin without-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
   service ssh restart
 }
 
