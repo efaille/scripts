fc9154470f0eec09b2385f08396c00bb026e3faa
Erick Faille <efaille@gmail.com>
2016-10-12 00:13:00 -0400

    fix pour le swap

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ upgrade_server() {
 increase_swap() {
   fallocate -l 4G /swapfile
   chmod 600 /swapfile
+  mkswap /swapfile
   swapon /swapfile
   echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 }
